faaf460f98a5779ba61fcec21a809f206d33cb8c
Kyle Drake <kyledrake@gmail.com>
2011-10-14 17:32:52 -0700

    Cleanups

--- geoloqi-sockets.js
+++ geoloqi-sockets.js
@@ -1,42 +1,27 @@
-geoloqi.Socket = function (type, auth, onLocation, onError) {
-  
+geoloqi.Socket = function (type, auth, onLocation, onDisconnect) {
+
   var object = function () {
     var self = this;
-
     var api = "https://subscribe.geoloqi.com:443";
-    
+
     this.auth = auth;
     this.type = type;
     this.onLocation = onLocation;
-    this.onError = onError;
-    
+    this.onDisconnect = onDisconnect;
+
     this.socket = io.connect(api);
 
     this.start = function(){
-    
       this.socket.on('enter authentication', function(data) {
         self.socket.emit('token', self.type+"-"+self.auth);
       });
 
       this.socket.on('location', function(data) {
-        console.log("location");
-        console.log('before');
-        console.log(data);
-        
-        var data = eval("("+data+")");
-        
-        console.log('after');
-        console.log(data);
-
+        var data = JSON.parse(data);
         self.onLocation(data);
-/*
-        if(isThereAnError){
-          self.onError(data);
-        } else {
-          self.onLocation(data);
-        }
-*/
-      });  
+      });
+
+      this.socket.on('disconnect', self.onDisconnect);
     };
 
   };
@@ -45,18 +30,3 @@ geoloqi.Socket = function (type, auth, onLocation, onError) {
 
   return new object();
 };
-
-/*
-var friendTrip = new geoloqi.Socket('trip', 'trip_id');
-friendTrip.onReceiveLocation = function(location) {
-  console.log('RECEIVED: '+location);
-};
-friendTrip.onReceiveError = function(error) {
-  console.log(error);
-    console.log('re trying');
-  friendTrip.start();
-};
-friendTrip.start();
-
-friendTrip.stop();
-*/
